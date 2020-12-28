17f332452f6a862109e6dbaa4c4095d73a84ab09
Touhid <touhidul15-7051@diu.edu.bd>
2020-12-28 16:16:53 +0600

    Serch with Multiple feild by Q Look ups

--- buildings/views.py
+++ buildings/views.py
@@ -4,6 +4,7 @@ from rest_framework.generics import ListAPIView,RetrieveAPIView
 from rest_framework.response import Response
 from .models import Home,ImageFiles
 from .serializers import HomeSerializer,HomeDetailSerializer,ImageFilesSerializer
+from django.db.models import Q
 
 
 class HomeListAPIView(ListAPIView):
@@ -16,9 +17,21 @@ class HomeDetailAPIView(RetrieveAPIView):
     serializer_class = HomeDetailSerializer
     queryset = Home.objects.filter(is_published=True).order_by('-list_date')
     lookup_field = 'slug'
+
 class ImageView(APIView):
     def get(self,request,pk,format=None):
         home=Home.objects.get(pk=pk)
         images=home.images.all()
         serializer=ImageFilesSerializer(images,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+#Multilevel/Field Search
+class Search(APIView):
+    permission_classes = (permissions.AllowAny,)
+    def post(self,request,format=None):
+        data=self.request.data
+        str=data['str'] #input from User 'str' is the key
+        q=(Q(description__icontains=str)) | (Q(title__icontains=str)) #Search by title and descreption fields
+        queryset=Home.objects.filter(is_published=True)
+        queryset=queryset.filter(q)
+        serializer=HomeSerializer(queryset,many=True)
+        return Response(serializer.data)
