691ff1064fe632e757c9f76f8d7ff69b8bcfab49
Touhid <touhidul15-7051@diu.edu.bd>
2021-01-09 18:15:02 +0600

    Intregrating 3rd party Post API(with Authentication)

--- frontend/views.py
+++ frontend/views.py
@@ -4,9 +4,21 @@ import requests, json
 def contact(request):
     if request.method=="POST":
         form=ContactForm(request.POST)
+        #For Authentication/Login
+        d={"username":"tou","password":"1234"}
+        url0="http://127.0.0.1:8000/api/token/"
+        api0=requests.post(url=url0,json=d)
+        try:
+            resp0=json.loads(api0.text)
+        except:
+            resp0=None
+        token=resp0['access']
+        print(token)
+
+        #Posting data to Database
         data=form.data
         url="http://127.0.0.1:8000/api/contact/"
-        api=requests.post(url=url,data=data)
+        api=requests.post(url=url,data=data,headers={'Authorization':f'Bearer {token}'})
         try:
             resp=json.loads(api.text)
 
